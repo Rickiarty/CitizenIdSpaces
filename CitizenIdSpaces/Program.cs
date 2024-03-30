07e49268a16a01c8433328518ac5db66a0893084
Rickiarty <fatalframe0719@gmail.com>
2024-03-30 09:24:00 +0800

    more LINQ

--- CitizenIdSpaces/Program.cs
+++ CitizenIdSpaces/Program.cs
@@ -4,23 +4,30 @@ using ROCCitizenIdSpace;
 Stopwatch sw = new();
 sw.Start();
 
-int count = 0;
-int total = 0;
-foreach (string possibleId in ROCIdSpace.PossibleIds)
-{
-	if (
+IEnumerable<string> filteredIds = ROCIdSpace.PossibleIds
+	.Where(possibleId => 
 		possibleId.Any(ch => ch == '2') &&
 		possibleId.Any(ch => ch == '6')
-		)
-	{
-		Console.Write($"{possibleId} : {ROCIdSpace.A_to_District[possibleId[0].ToString()]}\n");
-		count += 1;
-	}
-	total += 1;
+	); // Do NOT call '.ToList()' here. 
+int count = 0;
+int total = 0;
+foreach (string possibleId in filteredIds)
+{
+	Console.Write($"{possibleId} : {ROCIdSpace.A_to_District[possibleId[0].ToString()]}\n");
+	count += 1;
 }
-decimal ratio = (decimal)count / (decimal)total;
-Console.Write($"\ncount = {count}\ntotal = {total}\nratio = {ratio}\n");
 
 sw.Stop();
+TimeSpan t1 = sw.Elapsed;
 
-Console.WriteLine($"Δt (time delta) = {sw.Elapsed}");
+sw.Restart();
+
+total = ROCIdSpace.PossibleIds.Count();
+decimal ratio = (decimal)count / (decimal)total;
+Console.Write($"\ncount = {count}\ntotal = {total}\nratio = {ratio}\n\n");
+
+sw.Stop();
+TimeSpan t2 = sw.Elapsed;
+
+Console.WriteLine($"Δt1 (time delta) = {t1}");
+Console.WriteLine($"Δt2 (time delta) = {t2}");
